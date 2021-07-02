6387903f7561288fe2a65465eadfe3daf1e40f42
AinaRANDRIAMAMPITONY <ainarahajo12@gmail.com>
2021-07-02 15:48:21 +0300

    fix

---
+++ src/Shared/Modele/Enseignants.js
@@ -0,0 +1,15 @@
+export class Enseignants{
+    matricule =0;
+    nom ="";
+    prenom = "";
+    contact =[];
+
+
+    constructor(){
+        this.nom = "";
+        this.prenom = "";
+        this.email = "";
+        this.matricule =0;
+        this.contact =[];
+    }
+}
